acd416677fbe88ae71a63a1a9e9c18959206ca6b
yuanshenbo <283879750@qq.com>
2016-07-22 17:29:09 +0800

    test

--- PermissionsDispatcher-master/androidTraining/src/main/java/cn/com/hakim/androidtraining/MainActivity.java
+++ PermissionsDispatcher-master/androidTraining/src/main/java/cn/com/hakim/androidtraining/MainActivity.java
@@ -52,6 +52,12 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
